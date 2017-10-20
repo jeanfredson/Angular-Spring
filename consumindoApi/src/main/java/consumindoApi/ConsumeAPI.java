13cd0e0fe0e8d0106b51c23834882b1820157d03
Rafael <rapha.pse@outlook.com>
2017-10-20 12:37:53 -0200

    SpringBoot Quick Start - Desenvolvimento de aplicação para consumir API

---
+++ consumindoApi/src/main/java/consumindoApi/ConsumeAPI.java
@@ -0,0 +1,14 @@
+package consumindoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeAPI {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ConsumeAPI.class, args);
+	}
+
+}
