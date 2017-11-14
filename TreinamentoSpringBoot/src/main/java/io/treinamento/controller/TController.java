225e510ef74c166dde30c33751476cc52b7a56e2
Rafael <rapha.pse@outlook.com>
2017-11-14 15:54:46 -0200

    Treinamento com SpringBoot

--- TreinamentoSpringBoot/src/main/java/io/treinamento/controller/TController.java
+++ TreinamentoSpringBoot/src/main/java/io/treinamento/controller/TController.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +26,15 @@ public class TController {
 	public Topicos getTopicosID(@PathVariable String id){
 		return tpservice.getTopicosID(id);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/buscando")
+	public void addTopicos(@RequestBody Topicos t){
+		tpservice.addTopicos(t);
+	}
+	@RequestMapping(method=RequestMethod.PUT, value="/buscando/{id}")
+	public void updateTopicos(@RequestBody Topicos t, @PathVariable String id){
+		tpservice.updateTopicos(id, t);
+	}
+	
 
 }
--- TreinamentoSpringBoot/src/main/java/io/treinamento/service/TopicosService.java
+++ TreinamentoSpringBoot/src/main/java/io/treinamento/service/TopicosService.java
@@ -2,6 +2,7 @@ package io.treinamento.service;
 
 import io.treinamento.controller.Topicos;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class TopicosService {
 	
 	
-	private List<Topicos> top = Arrays.asList(
+	private List<Topicos> top = new ArrayList<Topicos>(Arrays.asList(
 				new Topicos("1", "Rafael-1", "Teste 001"),
 				new Topicos("2", "Rafael-2", "Teste 002"),
 				new Topicos("3", "Rafael-3", "Teste 003"),
@@ -19,7 +20,7 @@ public class TopicosService {
 				new Topicos("5", "Rafael-5", "Teste 005"),
 				new Topicos("6", "Rafael-6", "Teste 006"),
 				new Topicos("7", "Rafael-7", "Teste 007")
-		);
+		));
 	
 	public List<Topicos> getListar(){
 		return top;
@@ -27,7 +28,22 @@ public class TopicosService {
 	public Topicos getTopicosID(String id){
 		return top.stream().filter(t -> t.getId().equals(id)).findFirst().get();
 	}
+	
+	public void addTopicos(Topicos t){
+		top.add(t);
+	}
+	public void updateTopicos(String id, Topicos t) {
+		for(int i=0; i< top.size(); i++){
 		
+			Topicos topicos = top.get(i);
+			if(topicos.getId().equals(id)){
+				top.set(i, t);
+				return;
+			}
+		}
+		
+	}
+
 
 
 }
