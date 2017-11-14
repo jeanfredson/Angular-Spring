adaf472095b4fe11570a300bf297af9bc13edbda
Rafael <rapha.pse@outlook.com>
2017-11-14 15:29:17 -0200

    Treinamento com SpringBoot

--- TreinamentoSpringBoot/src/main/java/io/treinamento/controller/TController.java
+++ TreinamentoSpringBoot/src/main/java/io/treinamento/controller/TController.java
@@ -1,26 +1,28 @@
 package io.treinamento.controller;
 
-import java.util.Arrays;
+import io.treinamento.service.TopicosService;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TController {
 	
+	@Autowired
+	private TopicosService tpservice;
+	
 	@RequestMapping("/buscando")
 	public List<Topicos> getAllTopicos(){
-		return Arrays.asList(
-				new Topicos("1", "Rafael-1", "Teste 001"),
-				new Topicos("2", "Rafael-2", "Teste 002"),
-				new Topicos("3", "Rafael-3", "Teste 003"),
-				new Topicos("4", "Rafael-4", "Teste 004"),
-				new Topicos("5", "Rafael-5", "Teste 005"),
-				new Topicos("6", "Rafael-6", "Teste 006"),
-				new Topicos("7", "Rafael-7", "Teste 007")
-		);
+		return tpservice.getListar();
 		
+	}
+	@RequestMapping("/buscando/{id}")
+	public Topicos getTopicosID(@PathVariable String id){
+		return tpservice.getTopicosID(id);
 	}
 
 }
---
+++ TreinamentoSpringBoot/src/main/java/io/treinamento/service/TopicosService.java
@@ -0,0 +1,33 @@
+package io.treinamento.service;
+
+import io.treinamento.controller.Topicos;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicosService {
+	
+	
+	private List<Topicos> top = Arrays.asList(
+				new Topicos("1", "Rafael-1", "Teste 001"),
+				new Topicos("2", "Rafael-2", "Teste 002"),
+				new Topicos("3", "Rafael-3", "Teste 003"),
+				new Topicos("4", "Rafael-4", "Teste 004"),
+				new Topicos("5", "Rafael-5", "Teste 005"),
+				new Topicos("6", "Rafael-6", "Teste 006"),
+				new Topicos("7", "Rafael-7", "Teste 007")
+		);
+	
+	public List<Topicos> getListar(){
+		return top;
+	}
+	public Topicos getTopicosID(String id){
+		return top.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+		
+
+
+}
